04ed283ff1af5b019fb5940470b8313df6d70af8
B3L4CQU4 <gold1997ask@gmail.com>
2023-10-21 09:03:34 +0300

    pr 8

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,43 +9,27 @@ import PopupWithImage from './PopupWithImage.js';
 import PopupWithForm from './PopupWithForm.js';
 import UserInfo from './UserInfo.js';
 
-// находим попап редактирования профайла
+// Находим попап редактирования профайла и его элементы
 const profileEditPopup = document.querySelector('#profileEditPopup');
-// Находим форму редактирования профайла в DOM
 const profileFormElement = profileEditPopup.querySelector('#infoEditForm');
-// Находим поля формы редактирования профайла в DOM
 const nameInput = profileFormElement.querySelector('#nameInput');
 const jobInput = profileFormElement.querySelector('#jobInput');
-// находим кнопку редактировать в профайле
 const profileEditOpenBtn = document.querySelector('.profile').querySelector('.profile__edit-btn');
-// находим кнопку закрыть в попапе
-const profileEditCloseBtn = profileEditPopup.querySelector('.popup__close-btn');
-// находим профайл инфо
-const profileInfo = document.querySelector('.profile__info');
-// находим имя и описание
-const profileName = profileInfo.querySelector('.profile__name');
-const profileJob = profileInfo.querySelector('.profile__job');
 
-// находим попап добавления карточки
+// Находим попап добавления карточки и его элементы
 const addCardPopup = document.querySelector('#addCardPopup');
-// Находим форму добавления карточки в DOM
 const addCardFormElement = addCardPopup.querySelector('#addCardForm');
-// Находим поля формы добавления карточки в DOM
 const inputTitleFormAddCard = addCardFormElement.querySelector('#titleInput');
 const inputUrlFormAddCard = addCardFormElement.querySelector('#urlInput');
-// находим кнопку добавить в профайле
 const addCardBtn = document.querySelector('.profile').querySelector('.profile__add-btn');
-// находим кнопку закрыть в попапе
-const addCardCloseBtn = addCardPopup.querySelector('.popup__close-btn');
 
-const zoomPopup = document.querySelector('#popup_image');
-const zoomCloseButton = zoomPopup.querySelector('.popup__close-btn');
-
+// Функция для рендеринга карточки
 function cardRenderer(item) {
   const cardElement = createCard(item);
   cardSection.addItem(cardElement);
 }
 
+// Создание экземпляра UserInfo
 const userInfo = new UserInfo('.profile__name', '.profile__job');
 
 const imagePopup = new PopupWithImage('#popup_image', '.popup__image', '.popup__image-title');
@@ -79,14 +63,15 @@ const profileEditPopupInstance = new PopupWithForm('#profileEditPopup', (formDat
 
 profileEditPopupInstance.setEventListeners();
 
-// вызов функций переключения и обновления данных по кнопке редактировать профиль
+// Обработчики событий для попапов
 profileEditOpenBtn.addEventListener('click', () => {
   profileEditPopupInstance.open();
-  nameInput.value = userInfo.getUserInfo().name;
-  jobInput.value = userInfo.getUserInfo().job;
+  const userData = userInfo.getUserInfo();
+  nameInput.value = userData.name;
+  jobInput.value = userData.job;
   validatorProfileForm.resetValidationState();
 });
-// вызов функций переключения на клик по кнопке добавить
+
 addCardBtn.addEventListener('click', () => {
   addCardPopupInstance.open();
   validatorAddCardForm.resetValidationState();
@@ -95,11 +80,13 @@ addCardBtn.addEventListener('click', () => {
 const zoomPopupInstance = new Popup('#popup_image');
 zoomPopupInstance.setEventListeners();
 
-// включение валидации
+// Включение валидации
 const validatorProfileForm = new FormValidator(config, profileFormElement);
-validatorProfileForm .enableValidation();
+validatorProfileForm.enableValidation();
+
 const validatorAddCardForm = new FormValidator(config, addCardFormElement);
 validatorAddCardForm.enableValidation();
+
 
 
 
